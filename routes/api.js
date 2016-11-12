1e6faa8829cddaecc777672ebf1fbebeac4219c0
Robert Russo <rrusso44@gmail.com>
2016-11-12 16:47:01 -0500

    IT IS WORKING!!!

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,19 @@
 var express = require('express');
-// var spawn = require("child_process").spawn;
+var spawn = require("child_process").spawn;
 var multer = require('multer');
-var upload = multer({ dest:"uploads/" });
+var upload = multer({ dest:'uploads' });
 
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'This is the API' });
 });
 
-router.route('/upload').post(upload.single('avatar'), function(req, res, next) {
+router.route("/upload").post(upload.single('avatar'), function(req, res, next) {
   console.log(req.file);
+  spawn('python',['python/test.py']).stdout.on('data', function(data) {
+    res.end(data);
+  });
 });
 
 module.exports = router;
