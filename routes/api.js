5b64d31d14063b3c231f1ab64c0c083473fc5b2d
Robert Russo <rrusso44@gmail.com>
2016-11-12 15:28:11 -0500

    Fix for ROB

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,4 @@
 var express = require('express');
-var path = require('path');
-var formidable = require('formidable');
-var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
@@ -10,23 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/upload', function(req, res, next) {
-  var form = new formidable.IncomingForm();
-  form.multiples = false;
-  form.uploadDir = '/uploads';
-
-  form.on('file', function(fields, file) {
-    fs.rename(file.path, path.join(form.uploadDir, file.name));
-  });
-
-  form.on('error', function(err) {
-    console.log("ERROR: " + err);
-  });
-
-  form.on('end', function() {
-    res.end('success');
-  });
-
-  form.parse(req);
+  console.log(req.file);
 });
 
 module.exports = router;
