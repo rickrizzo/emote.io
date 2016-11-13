8aa7cf9be6ab6d7de802e54ac398ff6b02c5e6d6
paradc2 <paradc2@rpi.edu>
2016-11-13 04:32:01 -0500

    fix for rob

--- python/main.py
+++ python/main.py
@@ -42,12 +42,12 @@ img = img.resize((48, 48), Image.ANTIALIAS)
 
 # img = load_img('test_final2.png') 
 X_train = img_to_array(img)
-print X_train.shape
+# print X_train.shape
 X_train = np.mean(X_train, axis=2)
 # print X_train.shape
 # X_train = X_train.reshape(3, 48, 48)
 X_train = X_train.reshape((1,) + (48,48,1))
-print X_train.shape
+# print X_train.shape
 # print X_train.shape
 # X_train = X_train.reshape(X_train.shape[0], 48, 48, 3)
 X_train = X_train.astype("float32")
@@ -63,4 +63,4 @@ model = load_model('Deep_Net_Final.h5')
 
 # predict_classes(self, x, batch_size=32, verbose=1)
 predictions = model.predict_classes(X_train, batch_size=1, verbose=1)
-print predictions
+print predictions[0]
